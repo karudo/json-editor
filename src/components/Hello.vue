b7799a84d1a055f357713a50e990e601733e0e0e
Deonis Peretyagin <karudo@gmail.com>
2018-02-04 13:29:33 +0700

    add element ui

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    hello
+    <el-button type="success">Success</el-button>
+    <el-progress :percentage="70"/>
+    <el-date-picker
+      v-model="dt"
+      type="datetime"
+      placeholder="Select date and time">
+    </el-date-picker>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      dt: ''
+    }
+  }
+}
+</script>
