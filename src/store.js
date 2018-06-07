52e5165f6174b0dc1bbcc60a4b97b3f95e70c64f
Deonis Peretyagin <karudo@gmail.com>
2018-06-07 23:29:07 +0700

    json editor store module backbone

--- src/store.js
+++ src/store.js
@@ -3,7 +3,26 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  state: {}
+  state: {},
+  modules: {
+    jsonEditor: {
+      namespaced: true,
+      state: {
+
+      },
+      mutations: {
+        addEditor (state, {id, schema}) {
+
+        },
+        addElementToArray (state, {id, idx, element}) {
+
+        },
+        addPropToObject (state, {id, key, prop}) {
+
+        }
+      }
+    }
+  }
 })
 
 export default store
